8730bee46152c3cc2469e538d902e13d8728110e
MadLiy <lilya.emad34@icloud.com>
2025-01-08 17:30:05 +0100

    ajout d'observable, manipulation du watch et du watchEffect

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,24 @@
 <template>
-    <button @click="showTimer = !showTimer">Afficher / masquer</button>
-    <Timer v-if="showTimer"></Timer>
-    <Layout>
-        <template #header>
-            En tête
-        </template>
-        <template v-slot:aside>
-            Sidebar
-        </template>
-        <template v-slot:main>
-            Main
-        </template>
-        <template v-slot:footer>
-            Footer
-         </template>
-    </Layout>
-    <form action="" @submit.prevent="addTodo">
-        <fieldset role="group">
-            <input v-model="newTodo" type="text" placeholder="Tâche à effectuer">
-            <button :disabled="newTodo.length === 0">Ajouter</button>
-        </fieldset>
-    </form>
-  <h1>TODO-LIST</h1>
-    <div v-if="todos.length === 0"> Vous n'avez pas de tâches à faire :c </div>
-    <div v-else>
-        <ul>
-            <li
-                v-for="todo in sortedTodos"
-                :key="todo.date"
-                :class="{completed: todo.completed}">
-                <Checkbox :label="todo.title"
-                          v-model="todo.completed"
-                />
-
-            </li>
-        </ul>
-        <label>
-            <input type="checkbox" v-model="hideCompleted">
-            Masquer les tâches complétées
-        </label>
-        <p v-if="remainingTodos > 0">
-            {{ remainingTodos }} tâche{{ remainingTodos > 1 ? 's' : '' }} à faire
-        </p>
-        <Checkbox :label="'Bonjour'"/>
-    </div>
+    <input type="text" v-model="page.title">
+    Temps écoulé : {{ time }}
+    <button @click="reset">Reset</button>
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from 'vue'
-import Checkbox from './Checkbox.vue'
-import Button from '@/Button.vue'
-import Layout from '@/Layout.vue'
-import Timer from '@/Timer.vue'
+import { ref, watchEffect } from 'vue'
+import { useTimer } from '@/composable/useTimer.js'
 
-const newTodo = ref('');
-const todos = ref([]);
-const showTimer = ref(true);
-
-onMounted(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(r => r.json())
-
-        .then(v => todos.value = v.map(todo =>({ ...todo, date: todo.id})))
-
+const page = ref({
+    title: ''
 })
+const {time, reset} = useTimer()
 
-const hideCompleted = ref(false);
-const addTodo = () => {
-    todos.value.push({
-        title: newTodo.value,
-        completed: false,
-        date: Date.now()
-    })
-    newTodo.value = '';
-}
-
-const sortedTodos = computed(() => {
-    console.log('demo')
-    const sortedTodos =  todos.value.toSorted((a,b) => a.completed > b.completed ? 1 : -1);
-    if (hideCompleted.value === true){
-        return sortedTodos.filter(t => t.completed === false)
-    }
-    return sortedTodos;
-})
-
-const remainingTodos = computed(() => {
-    return todos.value.filter(t => t.completed === false).length;
+watchEffect(() => {
+    document.title = page.value.title;
 })
 
 </script>
 
 <style>
-.completed {
-    opacity: .5;
-    text-decoration: line-through;
-}
+
 </style>
